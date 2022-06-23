5a165e1cb43ccb3f44c0091e9d8d908e3e923326
CHANGHYEON LEE <chang.lee@morethani.com>
2022-06-23 14:21:21 +0900

    초기 파일 추가

---
+++ ngx/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @name android-packagemanager
+ * @description
+ * This plugin does something
+ *
+ * @usage
+ * ```typescript
+ * import { android-packagemanager } from '@awesome-cordova-plugins/android-packagemanager';
+ *
+ *
+ * constructor(private android-packagemanager: android-packagemanager) { }
+ *
+ * ...
+ *
+ *
+ * this.android-packagemanager.functionName('Hello', 123)
+ *   .then((res: any) => console.log(res))
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
+export declare class android {
+}
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+/**
+ * This is a template for new plugin wrappers
+ *
+ * TODO:
+ * - Add/Change information below
+ * - Document usage (importing, executing main functionality)
+ * - Remove any imports that you are not using
+ * - Remove all the comments included in this template, EXCEPT the @Plugin wrapper docs and any other docs you added
+ * - Remove this note
+ *
+ */
+import { Injectable } from '@angular/core';
+import { Plugin, Cordova, CordovaProperty, CordovaInstance, InstanceProperty, IonicNativePlugin } from '@ionic-native/core';
+import { Observable } from 'rxjs';
+
+@Plugin({
+  pluginName: 'cordova-plugin-android-packagemanager',
+  plugin: 'cordova-plugin-android-packagemanager',
+  pluginRef: 'cordova.plugins.android.packagemanager',
+  repo: '', // the github repository URL for the plugin
+  install: '', // OPTIONAL install command, in case the plugin requires variables
+  installVariables: [], // OPTIONAL the plugin requires variables
+  platforms: ['Android'],
+})
+@Injectable()
+export class AndroidPackageManager extends IonicNativePlugin {
+  /**
+   * 설치된 패키지명 목록을 가져온다.
+   */
+  @Cordova()
+  getInstalledPackages(): Promise<any> {
+    return; // We add return; here to avoid any IDE / Compiler errors
+  }
+
+}
